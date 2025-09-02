02ff78edb120431bcd6a68579ff961d0e85947ba
lephitar <philippe@phitar.com>
2025-09-02 22:46:51 +0200

    Clean

--- scripts/contracts.js
+++ scripts/contracts.js
@@ -3,6 +3,7 @@ import { TOKEN_ABI, VESTING_ABI } from "./abi.js";
 export function tokenContract(address, runner) {
   return new ethers.Contract(address, TOKEN_ABI, runner);
 }
+
 export function vestingContract(address, runner) {
   return new ethers.Contract(address, VESTING_ABI, runner);
 }
